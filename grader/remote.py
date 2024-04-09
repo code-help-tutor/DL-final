2f8509039ca49023eda87da04ef0b478f9d28a0e
Code Help Tutor <1823890830@qq.com>
2024-04-09 02:54:37 +0000

    WeChat cstutorcs DL final

---
+++ grader/remote.py
@@ -0,0 +1,36 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+from . import runner, utils
+
+# TODO: Wrap TeamRunner and Team in ray if possible
+try:
+    import ray
+
+    @ray.remote
+    class RayMatch(runner.Match):
+        pass
+
+    @ray.remote
+    class RayTeamRunner(runner.TeamRunner):
+        pass
+
+    @ray.remote
+    class RayStateRecorder(utils.StateRecorder):
+        pass
+
+    @ray.remote
+    class RayDataRecorder(utils.DataRecorder):
+        pass
+
+    @ray.remote
+    class RayVideoRecorder(utils.VideoRecorder):
+        pass
+
+    RayMatchException = ray.exceptions.RayTaskError
+
+    get = ray.get
+    init = ray.init
+
+except ImportError:
+    ray = None
